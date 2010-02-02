575eda975d11f2b02b6c8402fca8ad2424d816da
JP Hastings-Spital <jphastings@gmail.com>
2010-02-02 18:40:23 +0000

    Little niggles

--- irotoku_decode.c
+++ irotoku_decode.c
@@ -8,39 +8,37 @@ Details can be found here: http://creativecommons.org/licenses/by-nc-sa/2.0/uk/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "gd.h"
 
-int bits_per_colour, dont_stop_at_0;
+unsigned int bits_per_colour, dont_stop_at_0;
 
 void addToBuffer(int *buffer,int *bufferindex,int bits);
 
 int main(int argc, char **argv) {
 	gdImagePtr im;
 	FILE *in;
-	int x,y,rgb, opt, modulo;
+	unsigned int x,y,rgb, opt, modulo;
 	char *filename;
 	
 	dont_stop_at_0 = 0;
 	bits_per_colour = 1;
 	modulo = 2;
 	
-	while ((opt = getopt(argc, argv, "h0b:")) != -1) {
+	while ((opt = getopt(argc, argv, "0b:h")) != -1) {
 		switch (opt) {
 			case 'h':
-				fprintf(stdout,"For more info visit projects.kedakai.co.uk/irotoku\nUsage: %s [options] file.png ...\nWhere the options are one of:\n\t-0     Don't stop decoding at the first \\0 ASCII character\n\t-b N   Assume there are N bits per colour value. [1],2,4\nN.B. Output is sent to stdout.\n",argv[0]);
+				fprintf(stdout,"For more info visit projects.kedakai.co.uk/irotoku\nUsage: %s [options] file.png ...\nWhere the options are one of:\n\t-0     Don't stop decoding at the first \\0 ASCII character\n\t-b N   Assume there are N bits per colour value. [1],2,4...\nN.B. Output is sent to stdout.\n",argv[0]);
 				exit(0);
 			case 'b':
-				switch ((int)optarg[0]) {
-					case '4':
-						bits_per_colour = 4;
-						modulo = 16;
-						break;
-					case '2':
-						bits_per_colour = 2;
-						modulo = 4;
-						break;
+				sscanf(optarg,"%i",&bits_per_colour);
+				if (bits_per_colour > 8) {
+					bits_per_colour = 8;
+				} else if (bits_per_colour < 1) {
+					bits_per_colour = 1;
 				}
+				modulo = pow(2,bits_per_colour);
 				break;
 			case '0':
 				dont_stop_at_0 = 1;
@@ -50,6 +48,7 @@ int main(int argc, char **argv) {
 	if (argv[optind] == NULL) { fprintf(stderr,"You haven't specified an input file.\nTry %s -h for help.\n",argv[0]); exit(-1); }
 	filename = argv[optind];
 	
+	fprintf(stderr,"Processing %s\n %i bits per colour\n",filename,bits_per_colour);
 	int buffer = 0, bufferindex = 8;
 	
 	in =  fopen(filename, "r");
@@ -71,7 +70,6 @@ int main(int argc, char **argv) {
 }
 
 void addToBuffer(int *buffer,int *bufferindex,int bits) {
-	extern dont_stop_at_0,bits_per_colour;
 	
 	*bufferindex -= bits_per_colour;
 	*buffer += bits << *bufferindex;
